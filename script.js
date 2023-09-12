33765b0c8a592105d2c5577ee4eb6e3a6aa5056a
Markung555 <142219249+Markung555@users.noreply.github.com>
2023-09-12 20:36:31 +0700

    เทสระบบgit index.html

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const loginForm = document.getElementById("login-form");
+
+    loginForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        // เพิ่มโค้ดที่คุณต้องการในการตรวจสอบการเข้าสู่ระบบที่นี่
+
+        alert("ยินดีต้อนรับ! คุณได้เข้าสู่ระบบแล้ว");
+    });
+});
